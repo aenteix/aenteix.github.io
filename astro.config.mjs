107ba3b01b9c2492a747ccf9b49e947394d67741
aenteix <aenteix@gmail.com>
2026-03-11 10:39:10 +0000

    oo

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://aenteix.github.io',
-	base: '/aenteix.github.io',
+
 	integrations: [
 		starlight({
 			title: 'My Docs',
